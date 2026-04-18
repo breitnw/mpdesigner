86e2ba4b4e9705225fb1c39a4094dc0cf9743d13
Nick Breitling <breitling.nw@gmail.com>
2026-04-18 16:13:26 -0500

    delete flake comments

--- flake.nix
+++ flake.nix
@@ -41,37 +41,3 @@
       };
     });
 }
-
-# packages.sdl3-hs = hp.callCabal2nix
-#   "sdl3"
-#   (pkgs.fetchFromGitHub {
-#     owner = "klukaszek";
-#     repo = "sdl3-hs";
-#     rev = "d4a6b6e21f48cafb40028537a6cc9f55aa0943d3";
-#     sha256 = "sha256-0t4JcElzPLM4LmaJvn8S6VF1bdkeHoIbZcTSaPAynGI=";
-#   })
-#   { SDL3 = pkgs-unstable.sdl3.dev; };
-
-# buildInputs = [
-#   pkgs-unstable.sdl3.dev # want newer version of sdl3
-#   pkgs-unstable.sdl3-image.dev
-#   pkgs.alsa-lib.dev
-#   pkgs.libjack2.dev
-#   pkgs.pipewire.dev
-#   pkgs.libpulseaudio.dev
-#   pkgs.libx11.dev
-#   pkgs.libxcb.dev
-#   pkgs.libxau.dev
-#   pkgs.libxdmcp.dev
-#   pkgs.libxext.dev
-#   pkgs.libxcursor.dev
-#   pkgs.libxrender.dev
-#   pkgs.libxfixes.dev
-#   pkgs.libxi.dev
-#   pkgs.libxrandr.dev
-#   pkgs.libxscrnsaver # no .dev
-#   pkgs.libxtst
-#   pkgs.libdrm.dev
-#   pkgs.libgbm
-#   pkgs.waylandpp.dev
-# ];
