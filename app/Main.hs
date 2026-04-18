4d5a7f77fd4d6dd5aa32ad03d5a767c3c1652b35
Nick Breitling <breitling.nw@gmail.com>
2026-04-18 15:45:44 -0500

    sdl3-hs demo works

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,91 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import Control.Monad (unless)
-import Data.Word
-import SDL
-import SDL.Raw.Enum
+import Control.Monad
+import Foreign.Ptr (nullPtr)
+import SDL3
+import System.Exit (exitFailure, exitSuccess)
 
 main :: IO ()
 main = do
-  initializeAll
-  window <-
-    createWindow
-      "My SDL Application"
-      defaultWindow {windowInitialSize = V2 100 100, windowBorder = False}
-  windowOpacity window $= 0.3
-  renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer window 0
-  destroyWindow window
+  -- Check compiled version
+  sdlLog $ "Compiled SDL Version: " ++ show sdlVersion
+  when (sdlVersionAtLeast 3 3 0) $
+    sdlLog "Compiled with at least SDL 3.3.0"
 
-appLoop :: Renderer -> Window -> Word8 -> IO ()
-appLoop renderer window state = do
-  events <- pollEvents
-  let eventIsQPress event =
-        case eventPayload event of
-          KeyboardEvent keyboardEvent ->
-            keyboardEventKeyMotion keyboardEvent == Pressed
-              && keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
-          _ -> False
-      qPressed = any eventIsQPress events
-  -- rendererDrawBlendMode renderer $= SDL_BLENDMODE_ADD
-  rendererDrawColor renderer $= V4 (mod state 255) 0 80 10
-  clear renderer
-  present renderer
-  get (windowOpacity window) >>= print
-  delay 10
-  unless qPressed (appLoop renderer window (state + 1))
+  -- Get linked version
+  linkedVersion <- sdlGetVersion
+  sdlLog $ "Linked SDL Version: " ++ show linkedVersion
+
+  -- Get revision
+  revision <- sdlGetRevision
+  sdlLog $ "SDL Revision: " ++ revision
+
+  -- Set some basic metadata about our application
+  appMetadataSuccess <-
+    sdlSetAppMetadata
+      "SDL3 Haskell!"
+      "1.0.0"
+      "com.example.sdlapp"
+
+  unless appMetadataSuccess $ do
+    sdlLog "Failed to set app metadata!"
+
+  -- Add some additional metadata
+  _ <- sdlSetAppMetadataProperty propAppMetadataCreator "Kyle Lukaszek"
+  _ <- sdlSetAppMetadataProperty propAppMetadataCopyright "Copyright (c) 2025"
+  _ <- sdlSetAppMetadataProperty propAppMetadataType "Demo"
+
+  -- Print some metadata to verify it was set
+  name <- sdlGetAppMetadataProperty propAppMetadataName
+  sdlLog $ "App Name: " ++ maybe "Not set" id name
+
+  -- Initialize SDL with video and events subsystems
+  initSuccess <- sdlInit [SDL_INIT_VIDEO]
+
+  unless initSuccess $ do
+    sdlLog "Failed to initialize SDL!"
+    exitFailure
+
+  -- Check what subsystems are initialized
+  initializedSystems <- sdlWasInit []
+  sdlLog "Initialized subsystems:"
+  mapM_ printSubsystem initializedSystems
+
+  sdlLog "Would create window here..."
+  sdlLog "Application running..."
+
+  -- For the main thread callback, use a simpler callback
+  -- that doesn't try to print to avoid potential issues
+  isMain <- sdlIsMainThread
+  sdlLog $ "Are we on the main thread? " ++ show isMain
+
+  -- If we're already on the main thread, running on main thread is redundant
+  -- but we'll do it for demonstration
+  if isMain
+    then sdlLog "Already on main thread, calling directly:"
+    else sdlLog "Running callback on main thread:"
+
+  -- Use the simpler callback that doesn't do IO operations
+  _ <- sdlRunOnMainThread (\_ -> pure ()) nullPtr True
+  sdlLog "Callback completed"
+
+  sdlLog "Shutting down SDL..."
+  sdlQuit
+
+  sdlLog "Application terminated successfully"
+  exitSuccess
+
+-- Helper function to print subsystem names
+printSubsystem :: SDLInitFlags -> IO ()
+printSubsystem flag =
+  sdlLog $
+    "  - " ++ case flag of
+      SDL_INIT_AUDIO -> "Audio"
+      SDL_INIT_VIDEO -> "Video"
+      SDL_INIT_JOYSTICK -> "Joystick"
+      SDL_INIT_HAPTIC -> "Haptic"
+      SDL_INIT_GAMEPAD -> "Gamepad"
+      SDL_INIT_EVENTS -> "Events"
+      SDL_INIT_SENSOR -> "Sensor"
+      SDL_INIT_CAMERA -> "Camera"
+      _ -> "Unknown subsystem"
---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "Haskell development flake for mpdesigner";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.11";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      hp = pkgs.haskell.packages.ghc96;
+    in {
+      packages.default = hp.developPackage {
+        root = ./.;
+        withHoogle = true;
+        overrides = self: super: {
+          sdl3 = hp.callCabal2nix
+            "sdl3"
+            (pkgs.fetchFromGitHub {
+              owner = "klukaszek";
+              repo = "sdl3-hs";
+              rev = "d4a6b6e21f48cafb40028537a6cc9f55aa0943d3";
+              sha256 = "sha256-0t4JcElzPLM4LmaJvn8S6VF1bdkeHoIbZcTSaPAynGI=";
+            })
+            { SDL3 = pkgs-unstable.sdl3.dev; };
+        };
+        modifier = drv: pkgs.haskell.lib.addBuildTools drv [
+          hp.cabal-install
+          hp.haskell-language-server
+        ];
+      };
+    });
+}
+
+# packages.sdl3-hs = hp.callCabal2nix
+#   "sdl3"
+#   (pkgs.fetchFromGitHub {
+#     owner = "klukaszek";
+#     repo = "sdl3-hs";
+#     rev = "d4a6b6e21f48cafb40028537a6cc9f55aa0943d3";
+#     sha256 = "sha256-0t4JcElzPLM4LmaJvn8S6VF1bdkeHoIbZcTSaPAynGI=";
+#   })
+#   { SDL3 = pkgs-unstable.sdl3.dev; };
+
+# buildInputs = [
+#   pkgs-unstable.sdl3.dev # want newer version of sdl3
+#   pkgs-unstable.sdl3-image.dev
+#   pkgs.alsa-lib.dev
+#   pkgs.libjack2.dev
+#   pkgs.pipewire.dev
+#   pkgs.libpulseaudio.dev
+#   pkgs.libx11.dev
+#   pkgs.libxcb.dev
+#   pkgs.libxau.dev
+#   pkgs.libxdmcp.dev
+#   pkgs.libxext.dev
+#   pkgs.libxcursor.dev
+#   pkgs.libxrender.dev
+#   pkgs.libxfixes.dev
+#   pkgs.libxi.dev
+#   pkgs.libxrandr.dev
+#   pkgs.libxscrnsaver # no .dev
+#   pkgs.libxtst
+#   pkgs.libdrm.dev
+#   pkgs.libgbm
+#   pkgs.waylandpp.dev
+# ];
